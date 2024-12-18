8bcdbf592f7e0a78ed59f2dbc9da37dda6178fcd
Sophia Chirskaya <sophia.chirskaya@gmail.com>
2024-12-18 08:03:54 +0100

    js code

--- script.js
+++ script.js
@@ -12,5 +12,18 @@
 //se l'utente è over 65 applico lo sconto del 40%
 //se non è compreso in nessuno dei due casi precedenti ritorno il prezzo base
 
+let km = 60;
+let eta = 80;
 
+let prezzoBase = km * 0.21;
+
+if(eta>=0 && eta<= 18) {
+    let prezzo = prezzoBase - (prezzoBase * 0.2);
+    console.log(prezzo);
+} else if(eta>64) {
+    let prezzo = prezzoBase - (prezzoBase * 0.4);
+    console.log(prezzo);
+} else {
+    console.log(prezzoBase);
+}
 
